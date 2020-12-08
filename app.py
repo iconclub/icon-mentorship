188d95a56fe810ee4833cd25f5e5944fa2c60136
Hieu Nguyen <41591244+hieunguyen1053@users.noreply.github.com>
2020-12-08 14:59:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,18 @@ scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-while True:
-    time.sleep(1)
-    # Find a workbook by name and open the first sheet
-    # Make sure you use the right name here.
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1lOMM-RsbHL2sBaI-hk6GBuW6Ptc-qvjEb4HyiY2f2dc/edit#gid=1120776380").sheet1
-    # # Extract and print all of the values
-    data = sheet.get_all_records()
-    # reset
-    for idx, mentor in enumerate(mentors):
-        mentors[idx]['soNguoiChon'] = 0
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1lOMM-RsbHL2sBaI-hk6GBuW6Ptc-qvjEb4HyiY2f2dc/edit#gid=1120776380").sheet1
+# # Extract and print all of the values
+data = sheet.get_all_records()
+# reset
+for idx, mentor in enumerate(mentors):
+    mentors[idx]['soNguoiChon'] = 0
 
-    for d in data:
-        idx = [idx for idx, mentor in enumerate(mentors) if mentor['nickname'] == d['Chọn mentor']][0]
-        mentors[idx]['soNguoiChon'] += 1
+for d in data:
+    idx = [idx for idx, mentor in enumerate(mentors) if mentor['nickname'] == d['Chọn mentor']][0]
+    mentors[idx]['soNguoiChon'] += 1
 
-    with open("data.json", "w") as json_file:
-        json.dump(mentors, json_file, ensure_ascii=False)
+with open("data.json", "w") as json_file:
+    json.dump(mentors, json_file, ensure_ascii=False)
